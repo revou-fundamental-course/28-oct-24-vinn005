911d40577d12f82a334f1ccb5de7cbea62f1998a
vinn005 <davin.sariputra@gmail.com>
2024-11-06 21:51:46 +0700

    update 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // Ini Javascript
+
+console.log('Hello World');
+
+function validateForm() {
+    let beratBadan = document.getElementById('berat-badan').value;
+    // Kondisi Validasi jika form kosong
+    if (beratBadan == '' || parseInt(beratBadan) < 0) {
+        alert('Inputan Anda Kosong')
+    } else {
+        document.getElementById('result').innerHTML = beratBadan;
+    }
+    console.log(beratBadan);
+}
+
+console.log(document.getElementById('result').innerHTML);
