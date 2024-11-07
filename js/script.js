cfddcf3e1f3f59fb6e654912fb2521ff1bbaf7fb
vinn005 <davin.sariputra@gmail.com>
2024-11-07 23:21:20 +0700

    update tampilan fix

--- js/script.js
+++ js/script.js
@@ -4,13 +4,20 @@ console.log('Hello World');
 
 function validateForm() {
     let beratBadan = document.getElementById('berat-badan').value;
+    let tinggiBadan = document.getElementById('tinggi').value;
+    let usiaSekarang = document.getElementById('usia').value;
+
     // Kondisi Validasi jika form kosong
-    if (beratBadan == '' || parseInt(beratBadan) < 0) {
-        alert('Inputan Anda Kosong')
+    if (beratBadan == '' || tinggiBadan == '' || usiaSekarang == '' || parseInt(beratBadan, tinggiBadan, usiaSekarang) <= 1) {
+        alert('Inputan Anda Tidak Valid')
+    
     } else {
-        document.getElementById('result').innerHTML = beratBadan;
+        document.getElementById('result').innerHTML = (beratBadan);
     }
+       
+    }
+    
     console.log(beratBadan);
-}
+
 
 console.log(document.getElementById('result').innerHTML);
